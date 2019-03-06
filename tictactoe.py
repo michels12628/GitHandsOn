b232c57facaa41f4b2441a2d3b4f50eabbf14990
[omg-vicky] <[victoria.tran@csu.fullerton.edu]>
2019-03-06 15:29:09 -0500

    Added print_board, select_char and can_move functions

--- tictactoe.py
+++ tictactoe.py
@@ -16,14 +16,27 @@ winners = ((0,1,2),(3,4,5),(6,7,8), (0,3,6), (1,4,7), (2,5,8),(0,4,8),(2,4,6))
 tab=range(1,10)
 
 def print_board():
-	#write the function logic to display the board
-        print(char,end=end)
-        
+	x=1
+	for i in board:
+	end ='l'
+	if x%3 ==0:
+		end = '\n' 
+		if i !=1: end += '------\jn';
+	   char ''
+	    if i in ('X','O'): char =i;
+	    x+=1
+	    print(char,end =end)
+
 def select_char():
-    return chars
+	chars=('X','O')
+	if random.randint(0,1) ==0:
+	    return chars[::-1]
+	     return chars
 
 def can_move(brd, player, move):
-    return False
+ 	if move in tab and brd[move-1] == move-1:
+		return True
+	     return False
 
 def can_win(brd, player, move):
     return win
