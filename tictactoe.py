dca7706ffa02a4ec1501fd4f5510cdb5995f9249
[omg-vicky] <[victoria.tran@csu.fullerton.edu]>
2019-03-06 16:24:28 -0500

    final patch

--- tictactoe.py
+++ tictactoe.py
@@ -66,29 +66,48 @@ def make_move(brd, player, move, undo=False):
 
 # AI goes here
 def computer_move():
-	move =-1
-	    # If I can win, others don't matter.
-		for i in range(1,10):
-		if make_move(board,computer,i,True)[1]:
-			move=i
-			break
-		if move == -1:
-		# If player can win, block him.
-		for i in range (1,10):
-		if make_move(board,player,i,True)[1]:
-		move =i
-		break
-		if move ==-1:
-		# Otherwise, try to take one of desired places.
-		for tup in moves:
-		for mv in tup:
-		if move ==-1 and can_move(board, computer,mv):
-			move =mv
-			break
-		return make_move(board,computer,move)
+    move=-1
+    # If I can win, others don't matter.
+    for i in range(1,10):
+        if make_move(board, computer, i, True)[1]:
+        	move=i
+        	break
+    if move == -1:
+        # If player can win, block him.
+        for i in range(1,10):
+        	if make_move(board, player, i, True)[1]:
+            	move=i
+            	break
+    if move == -1:
+        # Otherwise, try to take one of desired places.
+        for tup in moves:
+        	for mv in tup:
+            	if move == -1 and can_move(board, computer, mv):
+                	move=mv
+                	break
+    return make_move(board, computer, move)
+ 
 
 def space_exist():
     return board.count('X') + board.count('O') != 9
+player, computer = select_char()
+print('Player is [%s] and computer is [%s]' % (player, computer))
+result='%%% Deuce ! %%%'
+while space_exist():
+    print_board()
+    print('# Make your move ! [1-9] : ', end='')
+    move = int(input())
+    moved, won = make_move(board, player, move)
+    if not moved:
+        print(' >> Invalid number ! Try again !')
+        continue
+    #
+    if won:
+        result='*** Congratulations ! You won ! ***'
+        break
+    elif computer_move()[1]:
+        result='=== You lose ! =='
+        break;
 
 player, computer = select_char()
 
